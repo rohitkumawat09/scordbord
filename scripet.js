0886532ed6e6ae7705d5e5360956106168180252
Your Name <your-email@example.com>
2025-03-21 03:35:04 -0700

    first commit

--- scripet.js
+++ scripet.js
@@ -1,108 +1,124 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const fullname = document.querySelector("#fname");
-    const lastname = document.querySelector("#lname");
-    const countries = document.querySelector("#countries");
-    const score = document.querySelector("#score");
-    const container = document.querySelector(".score-container");
-    const btn = document.querySelector("#btn");
-    let scorearray = [];
-    console.log(scorearray);
 
-    if (btn) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-            if (!fullname.value || !lastname.value || !countries.value || !score.value) {
-                alert("Please fill in all fields ");
-                return;
-            }
-            const scoreValue = Number(score.value);
 
-            let obj1 = {
-                id: scorearray.length + 1,
-                fname: fullname.value,
-                lname: lastname.value,
-                countries: countries.value,
-                score: scoreValue,
-            };
 
-            scorearray.push(obj1);
-            name();
-            displayScores();
-        });
+const form = document.querySelector("form");
+
+const fname = document.querySelector("#fname");
+const lname = document.querySelector("#lname");
+const score = document.querySelector("#score");
+const country = document.querySelector("#country");
+
+const resultsDiv = document.querySelector("#results");
+
+const data = [];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (!fname.value || !lname.value || !country.value || !score.value || isNaN(score.value)) {
+        alert("Please fill in all fields with a valid score.");
+        return;
     }
+    const obj = {};
+    obj.name = fname.value + " " + lname.value;
+    obj.country = country.value;
+    obj.score =Number( score.value);
 
-    function name() {
-        scorearray.sort((a, b) => b.score - a.score);
-    }
+    data.push(obj);
 
-    function displayScores() {
-        container.innerHTML = "";
+    fname.value = "";
+    lname.value = "";
+    score.value = "";
+    country.value = "";
+    console.log(data);
+    showData(data);
+    sortData()
 
-        scorearray.forEach((obj1) => {
-            const scoreCard = document.createElement("div");
-            scoreCard.classList.add("score-card");
-
-            const name = document.createElement("p");
-            name.innerHTML = `${obj1.fname} ${obj1.lname}`;
-
-            const country = document.createElement("p");
-            country.innerHTML = `Country: ${obj1.countries}`;
-
-            const scoreValue = document.createElement("p");
-            scoreValue.innerHTML = `Score: ${obj1.score}`;
-
-            const deleteBtn = document.createElement("button");
-            deleteBtn.innerHTML = "Delete";
-            deleteBtn.classList.add("delete-btn");
-            deleteBtn.addEventListener("click", () => deleteScore(obj1, scoreCard));
-
-            const inkerement = document.createElement("button");
-            inkerement.innerHTML = "+5";
-            inkerement.addEventListener("click", () => inkrementScore(obj1, scoreValue));
-
-            const deckrement = document.createElement("button");
-            deckrement.innerHTML = "-5";
-            deckrement.addEventListener("click", () => deckrementScore(obj1, scoreValue));
-
-            scoreCard.append(name, country, scoreValue, inkerement, deckrement, deleteBtn);
-            container.append(scoreCard);
-        });
-
-        fullname.value = "";
-        lastname.value = "";
-        score.value = "";
-        countries.value = "Choose Country";
-    }
-
-    function deleteScore(obj, card) {
-        scorearray = scorearray.filter((item) => item !== obj);
-        name()
-
-        card.remove();
-    }
-
-    function inkrementScore(obj, scoreValueElement) {
-        obj.score += 5;
-        scoreValueElement.innerHTML = `Score: ${obj.score}`;
-        name();
-        // inkrementScore();
-        displayScores();
-    }
-
-    function deckrementScore(obj, scoreValueElement) {
-
-
-
-        if (obj.score - 5 >= 0) {
-            obj.score -= 5;
-            scoreValueElement.innerHTML = `Score: ${obj.score}`;
-        } else {
-            alert("Score cannot be less than 0.");
-        }
-        // obj.score -=5;
-        // scoreValueElement.innerHTML = `Score: ${obj.score}`;
-        // name();
-        // deckrementScore();
-        displayScores();
-    }
 });
+
+function sortData() {
+    data.sort((a, b) => b.score - a.score); // Sort by score in descending order
+    showData(data); // Show sorted data
+}
+// function sortData() {
+//     data.sort((a, b) => {    b.score - a.score; 
+//        return
+
+//     });
+//     sortData(data)
+
+// }
+
+
+
+
+function showData(arr) {
+    resultsDiv.innerHTML = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        const parent = document.createElement("div");
+        const name = document.createElement("p");
+        const score = document.createElement("p");
+        const country = document.createElement("p");
+        const plusFive = document.createElement("span");
+        const minusFive = document.createElement("span");
+        const del = document.createElement("button");
+
+        parent.classList.add("data");
+
+        name.innerText = arr[i].name;
+        score.innerText = arr[i].score;
+        country.innerText = arr[i].country;
+
+        plusFive.innerText = "+5";
+        minusFive.innerText = "-5";
+        del.innerText = "Delete";
+
+        plusFive.addEventListener("click", () => {
+            addFive(i);
+        });
+
+        minusFive.addEventListener("click", () => {
+            subtractFive(i);
+        });
+
+        del.addEventListener("click", () => {
+            deleteData(i);
+        });
+
+        const actions = document.createElement("div");
+        actions.append(plusFive, minusFive, del);
+
+        parent.append(name, score, country, actions);
+        resultsDiv.append(parent);
+    }
+}
+
+
+
+
+function deleteData(index) {
+    data.splice(index, 1);
+    showData(data);
+    sortData() 
+}
+
+// function deleteData(value) {
+//     data.remove(value);  
+//     showData(data); 
+// }
+
+function addFive(index) {
+    data[index].score = Number(data[index].score) + 5;
+    showData(data);
+    sortData() 
+}
+
+function subtractFive(index) {
+    if (data[index].score >= 5) {
+        data[index].score = Number(data[index].score) - 5;
+        showData(data);
+        sortData();
+    } else {
+        alert("Score cannot be less than 0.");
+    }
+}
